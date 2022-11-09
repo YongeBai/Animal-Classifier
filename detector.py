9382c514919b77a0e97d63e6da7bb16eebd185c5
Yonge Bai <97137323+YongeBai@users.noreply.github.com>
2022-11-09 18:54:17 -0500

    almost done

--- detector.py
+++ detector.py
@@ -1,9 +1,13 @@
 import torch
 import cv2 as cv
 import numpy as np
+import os
 
 class AnimalCLassifier:
     def __init__(self) -> None:
+
+        self.animals = []
+
         self.model = torch.hub.load(
             'ultralytics/yolov5', 
             'yolov5s', 
@@ -14,15 +18,26 @@ class AnimalCLassifier:
         self.classes = self.model.names
         self.model.multi_label = False 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print("Device: ", self.device)
+
+    def label_to_class(self, label):
+        return self.classes[(int(label))]
+
+    def isAnimal(self, label):
+        if 14 <= label <= 23:
+            return True
+        else:
+            return False
 
 
-    def detect(self, input_image):
-        img_bgr = cv.imread(input_image)
+    def detect(self, folder, input_image):
+        img_bgr = cv.imread((os.path.join(folder,input_image)))
         img_rgb = cv.cvtColor(img_bgr, cv.COLOR_BGR2RGB)
         results = self.model(img_rgb)
-        results.show()
+        label = results.xyxyn[0][:, -1]
+        prediction = self.label_to_class(label)
+        animal = self.isAnimal(label)
+        
+        return (prediction, animal)
 
-
-detector = AnimalCLassifier()
-detector.detect("cat.png")
+# detector = AnimalCLassifier()
+# print(detector.detect("cat.png"))
